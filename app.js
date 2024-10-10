efc1d86fdd15966cabee5e159443729f2cd192d7
Ornelle <ornellechougourou@gmail.com>
2024-10-10 18:38:40 +0200

    Project Structure+Database Connexction test

--- app.js
+++ app.js
@@ -1,11 +1,28 @@
-const express = require('express')
-const app = express()
-const port = 3000
+require('dotenv').config();
+const mongoose=require('mongoose')
+const express = require('express');
+const cors = require('cors');
+const app = express();
 
+//const apiRouter = require('./routes')
+app.use(cors());
+
+app.use(express.json());
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.send('Serveur fonctionne');
+});
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+//app.use('/api/', apiRouter);
+
+app.listen(3000, () => {
+    console.log('server is running');
+
+    mongoose.connect(process.env.MONGO_URL)
+        .then(() => {
+            console.log('Database connected');
+        })
+        .catch((error) => {
+            console.log("Error when trying to connect to the database:", error);
+        });
 })
+module.exports = app;
